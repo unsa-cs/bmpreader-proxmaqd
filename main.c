285c4873e127d5603a0758c53a301bf95e485c4d
Marcelo Quina <mquinad@unsa.edu.pe>
2024-09-27 19:25:24 -0500

    lectura del 3 argc para q se detecte q se quiere en blanco y negro sin el string.h

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "bmp.h"
 #include <GL/glut.h>
 #include <stdio.h>
-#include <string.h>
 
 BMPImage *image;
 
@@ -27,20 +26,35 @@ int main(int argc, char *argv[]) {
   char filename[256];
   int i = 0;
 
-  while(argv[1][i]!='\0' && i < sizeof(filename)-1){
-    filename[i]=argv[1][i];
+  while (argv[1][i] != '\0' && i < sizeof(filename) - 1) {
+    filename[i] = argv[1][i];
     i++;
   }
-  filename[i]='\0';
+  filename[i] = '\0';
 
   image = readBMP(filename);
   if (!image) return 1;
 
-  if (argc == 3 && strcmp(argv[2], "bn") == 0) {
-    convertirabn(image);
+  if (argc == 3) {
+    int is_bw = 1;
+    char *bw_id = argv[2];
+
+    char bw_text[] = "bw";
+    for (i = 0; bw_text[i] != '\0' && bw_id[i] != '\0'; i++) {
+      if (bw_text[i] != bw_id[i]) {
+        is_bw = 0; 
+        break;
+      }
+    }
+    if (bw_text[i] != '\0' || bw_id[i] != '\0') {
+      is_bw = 0;
+    }
+
+    if (is_bw) {
+      convertirabn(image);
+    }
   }
 
-  // Establecer el modo de visualización
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
   glutInitWindowSize(image->width, image->height);
   glutCreateWindow("Visualizador de BMP");
